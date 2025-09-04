f161c554b93582ccb2dfc7e6db54ac3a18c13438
Gytis <medelis.gytis@gmail.com>
2025-09-05 00:38:55 +0300

    deploy for github pages configuration

--- frontend/src/assets/js/configs.ts
+++ frontend/src/assets/js/configs.ts
@@ -1,4 +1,5 @@
-const isBackendOn = true;
+const isBackendOn =
+  import.meta.env.VITE_BACKEND === undefined ? true : import.meta.env.VITE_BACKEND === "true";
 const githubLink = "https://github.com/GymMed/Picture-Sizer";
 
 export { isBackendOn, githubLink };
