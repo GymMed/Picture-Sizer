61d2349c7750d8ebc963bf15d674db7ffeca11e1
Gytis <medelis.gytis@gmail.com>
2025-09-05 01:15:23 +0300

    fixed vite builds for backend and github separation, fixed readme gif link

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,23 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
-  base: "public/",
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  const isBackendOn = env.VITE_BACKEND === "true";
+  const baseUrl = isBackendOn ? "public/" : "/Picture-Sizer/";
+
+  return {
+    plugins: [vue(), vueDevTools(), tailwindcss()],
+    base: baseUrl,
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
+  };
 });
